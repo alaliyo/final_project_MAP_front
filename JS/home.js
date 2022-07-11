4579ff25d4f9d81404560511fce0274f83a8e00d
github username <github>
2022-07-11 11:29:55 +0900

    feat : 닉네님 중복 조회 시 서버에서 반환되는 값이 변경되서 코드 수정

--- JS/home.js
+++ JS/home.js
@@ -3,6 +3,24 @@ $(window.document).ready(function() {
     cards();
 })
 
+// function time2str(date) {
+//     let today = new Date()
+//     let time = (today - date) / 1000 / 60  // 분
+
+//     if (time < 60) {
+//         return parseInt(time) + "분 전"
+//     }
+//     time = time / 60  // 시간
+//     if (time < 24) {
+//         return parseInt(time) + "시간 전"
+//     }
+//     time = time / 24
+//     if (time < 14) {
+//         return parseInt(time) + "일 전"
+//     }
+//     return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+// }
+
 function cards() {
     $('#cards').empty()
     $.ajax({
