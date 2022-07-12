37e5e9eaa4e87a5e92445418808d226b5966ad0c
github username <github>
2022-07-12 20:40:50 +0900

    feat : 백앤드 아이디, 닉네임 조회 반환값 변경으로 인한 코드 수정

--- JS/login.js
+++ JS/login.js
@@ -218,7 +218,7 @@ function username_check_dup() {
         contentType: "application/json; charset=UTF-8",
         success: function (response) {
             console.log(response)
-            if (response == "중복 유저 네임입니다") {
+            if (response == "회원 정보 중복입니다.") {
                 $("#help-id").text("이미 존재하는 아이디입니다.").removeClass("is-safe").addClass("is-danger")
                 $("#help-id").focus()
             }
@@ -259,13 +259,13 @@ function nickname_check_dup() {
         contentType: "application/json; charset=UTF-8",
         success: function (response) {
             console.log(response)
-            if (response == "중복된 닉네임입니다") {
-                $("#help-nickname").text("이미 존재하는 닉네임입니다.").removeClass("is-safe").addClass("is-danger")
-                $("#input-nickname").focus()
-            }
-            else{
+            if (response == "회원 정보 중복입니다.") {
                 $("#help-nickname").text("사용할 수 있는 닉네임입니다.").removeClass("is-danger").addClass("is-success")
                 $("#help-nickname").removeClass("is-loading")
+            }
+            else{
+                $("#help-nickname").text("이미 존재하는 닉네임입니다.").removeClass("is-safe").addClass("is-danger")
+                $("#input-nickname").focus()
             }
         }
     });
